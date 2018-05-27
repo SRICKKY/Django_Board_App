9f6d9624659dbbb1dbf973a4f9e163609acbb1d9
Srikant <kumar_srikant@outlook.com>
2018-05-27 13:28:01 +0530

    Added BoardTopicsTest

--- Django_Boards/urls.py
+++ Django_Boards/urls.py
@@ -5,5 +5,6 @@ from boardsapp import views
 
 urlpatterns = [
 	path('',views.home, name='home'),
+	path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
 ]
